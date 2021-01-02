62d396002f393fb2cac7f9c2a5aca43e60d5b058
Q118 <shelbyfish91@gmail.com>
2021-01-01 20:00:34 -0800

    handleMove fucntionality

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,39 @@ const App: React.FC = () => {
 
 const [fen, setFen] = useState(chess.fen());
 
+const handleMove = (move: ShortMove) => {
+  if(chess.move(move)) {
+    setTimeout(() => {
+      const moves = chess.moves();
+
+      if (moves.length > 0) {
+        const computerMove= moves[Math.floor(Math.random() * moves.length)];
+        chess.move(computerMove);
+        setFen(chess.fen());
+        }
+    }, 300);
+    
+    setFen(chess.fen());
+  }
+};
+
 
 	return (
-		<div className="flex-center">
-			<h1>Random Chess</h1>
-			<Chessboard
-				width={400}
-				position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-			/>
-		</div>
-	);
+    <div className="flex-center">
+      <h1>Random Chess</h1>
+      <Chessboard
+        width={400}
+        position={fen}
+        onDrop={(move) =>
+          handleMove({
+            from: move.sourceSquare,
+            to: move.targetSquare,
+            promotion: "q",
+          })
+        }
+      />
+    </div>
+  );
 };
 
 export default App;
