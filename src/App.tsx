b4b8bcec0c26f16ba59e869ac0ea73957fcf1467
Q118 <shelbyfish91@gmail.com>
2021-01-01 19:21:33 -0800

    chessInstance

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
+/** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Chessboard from "chessboardjsx";
+import { ChessInstance, ShortMove } from "chess.js";
+
+const Chess = require("chess.js");
 
 const App: React.FC = () => {
-  return (
-    <div className="flex-center">
-      <h1>Random Chess</h1>
-      <Chessboard
-        width={400}
-        position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-      />
-    </div>
-  );
+	const [chess] = useState<ChessInstance>(
+		new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+	);
+
+  
+
+	return (
+		<div className="flex-center">
+			<h1>Random Chess</h1>
+			<Chessboard
+				width={400}
+				position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+			/>
+		</div>
+	);
 };
 
 export default App;
